3ba02a973b50cef191a71d51bfcf4432b3779b47
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-09-27 21:37:31 +0100

    feat: Created get and post requests to add a new menu item to a restaurant

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -48,9 +48,11 @@ def home_page():
 
     restaurants = session.query(Restaurant).all()
     restaurants_html = "".join(
-        restaurant_html.format(restaurant_name=restaurant.name, restaurant_id=restaurant.id) for restaurant in restaurants
+        restaurant_html.format(restaurant_name=restaurant.name, restaurant_id=restaurant.id) for restaurant in
+        restaurants
     )
     return page_html.format(restaurants=restaurants_html)
+
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurant_menu(restaurant_id):
@@ -66,10 +68,13 @@ def restaurant_menu(restaurant_id):
                     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
                 </head>
                 <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
-                    <a href="/" style="color: #0083a8; font-size: 20px; margin-bottom: 5px;">
+                    <a href="/r" style="color: #0083a8; font-size: 20px; margin-bottom: 5px;">
                         Home
                     </a>
                     <h1>{name}</h1>
+                    <a href="/restaurants/{restaurant_id}/new_menu_item/" style="color: #0083a8; font-size: 20px; margin-bottom: 5px;">
+                        Add menu item
+                    </a>
                     <ul style="padding: 0;">
                         {menu}
                     </ul>
@@ -92,25 +97,109 @@ def restaurant_menu(restaurant_id):
     menu_html = ''.join(
         menu_item_html.format(name=item.name, price=item.price, description=item.description) for item in items
     )
-    return page_html.format(name=restaurant.name, menu=menu_html)
+    return page_html.format(name=restaurant.name, menu=menu_html, restaurant_id=restaurant_id)
+
 
 # Task 1: Create route for newMenuItem function here
 
-
+@app.route('/restaurants/<int:restaurant_id>/new_menu_item/', methods=['GET', 'POST'])
 def new_menu_item(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if(request.method == 'POST'):
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        form_complete = bool(name) & bool(price) & bool(description)
+        if(form_complete):
+            menu_item = MenuItem(name=name, price=price, description=description, restaurant=restaurant)
+            session.add(menu_item)
+            session.commit()
+        return redirect('/restaurants/{restaurant_id}/'.format(restaurant_id=restaurant_id))
+    else:
+        page_html = '''
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Restaurants</title>
+                    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+                </head>
+                <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
+                    <h1>{restaurant_name}</h1>
+                    <form style="margin-bottom: 10px;" method="POST" action="/restaurants/{restaurant_id}/new_menu_item/">
+                        <h2>Add menu item</h2>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Enter name"
+                            style="display: block;
+                                width: 100%;
+                                max-width: 500px;
+                                margin-bottom: 10px;
+                                font-size: 16px;
+                                padding: 5px;
+                                border-radius: 3px;
+                                border: 1px solid lightgray;"
+                        >
+                        <input
+                            type="text"
+                            name="price"
+                            placeholder="Enter price"
+                            style="display: block;
+                                width: 100%;
+                                max-width: 500px;
+                                margin-bottom: 10px;
+                                font-size: 16px;
+                                padding: 5px;
+                                border-radius: 3px;
+                                border: 1px solid lightgray;"
+                        >
+                        <textarea
+                            name="description"
+                            placeholder="Enter description"
+                            style="display: block;
+                                width: 100%;
+                                max-width: 500px;
+                                margin-bottom: 5px;
+                                font-size: 16px;
+                                padding: 5px;
+                                border-radius: 3px;
+                                border: 1px solid lightgray;"
+                        ></textarea>
+                        <button
+                            type="submit"
+                            style="font-size: 16px;
+                                margin: 10px;
+                                background-color: #0083a8;
+                                padding: 5px 10px;
+                                border: 0;
+                                border-radius: 3px;
+                                color: white;
+                                font-weight: 300;"
+                        >Create</button>
+                    </form>
+                    <a style="color: #0083a8; margin: 5px; font-weight: 400;" href="/restaurants/{restaurant_id}/">
+                        Cancel
+                    </a>
+                </body>
+            </html>
+        '''
+        return page_html.format(restaurant_id=restaurant_id, restaurant_name=restaurant.name)
+
 
 # Task 2: Create route for editMenuItem function here
 
-
 def edit_menu_item(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
+
 
 # Task 3: Create a route for deleteMenuItem function here
 
 
 def delete_menu_item(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
