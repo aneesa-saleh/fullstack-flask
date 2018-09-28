391c61ea44ef2b3a2a21bef2b2a84fe2cbb63b28
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-09-28 06:33:04 +0100

    feat: Created get and post requests to edit restaurant menu items

--- project.py
+++ project.py
@@ -92,10 +92,16 @@ def restaurant_menu(restaurant_id):
             <span style="display: block; font-weight: 400; font-size: 22px; margin-bottom: 10px;">{name}</span>
             <span style="display: block; font-size: 16px; margin-bottom: 10px;">Price: {price}</span>
             <span style="display: block; font-size: 18px;">{description}</span>
+            <a href="/restaurants/{restaurant_id}/{id}/edit_menu_item/"
+                style="display: inline-block; color: #0083a8; font-size: 18px; margin: 10px 0; text-decoration: none;"
+            >
+                Edit
+            </a>
         </li>
         '''
     menu_html = ''.join(
-        menu_item_html.format(name=item.name, price=item.price, description=item.description) for item in items
+        menu_item_html.format(name=item.name, price=item.price, description=item.description, id=item.id, restaurant_id=restaurant_id)
+        for item in items
     )
     return page_html.format(name=restaurant.name, menu=menu_html, restaurant_id=restaurant_id)
 
@@ -190,8 +196,111 @@ def new_menu_item(restaurant_id):
 
 # Task 2: Create route for editMenuItem function here
 
-def edit_menu_item(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_item_id>/edit_menu_item/', methods=['GET', 'POST'])
+def edit_menu_item(restaurant_id, menu_item_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menu_item = session.query(MenuItem).filter_by(id=menu_item_id).one()
+    print('before:', menu_item.name, menu_item.id)
+    print(request.method)
+    if (request.method == 'POST'):
+        print('Im a post')
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+        form_complete = bool(name) & bool(price) & bool(description)
+        print('pre-after:', menu_item.name, menu_item.id)
+        if (form_complete):
+            menu_item.name = name
+            menu_item.price = price
+            menu_item.description = description
+            print('after:')
+            print(menu_item.name, menu_item.id)
+            #session.add(menu_item)
+            #session.commit()
+        else:
+            print('nope')
+        return redirect('/restaurants/{restaurant_id}/'.format(restaurant_id=restaurant_id))
+    else:
+        print('Im a get')
+        page_html = '''
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>Restaurants</title>
+                        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+                    </head>
+                    <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
+                        <h1>{restaurant_name}</h1>
+                        <form style="margin-bottom: 10px;" method="POST" action="/restaurants/{restaurant_id}/{menu_item_id}/edit_menu_item/">
+                            <h2>Edit {menu_item_name}</h2>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Enter name"
+                                value="{menu_item_name}"
+                                style="display: block;
+                                    width: 100%;
+                                    max-width: 500px;
+                                    margin-bottom: 10px;
+                                    font-size: 16px;
+                                    padding: 5px;
+                                    border-radius: 3px;
+                                    border: 1px solid lightgray;"
+                            >
+                            <input
+                                type="text"
+                                name="price"
+                                placeholder="Enter price"
+                                value="{menu_item_price}"
+                                style="display: block;
+                                    width: 100%;
+                                    max-width: 500px;
+                                    margin-bottom: 10px;
+                                    font-size: 16px;
+                                    padding: 5px;
+                                    border-radius: 3px;
+                                    border: 1px solid lightgray;"
+                            >
+                            <textarea
+                                name="description"
+                                placeholder="Enter description"
+                                style="display: block;
+                                    width: 100%;
+                                    max-width: 500px;
+                                    margin-bottom: 5px;
+                                    font-size: 16px;
+                                    padding: 5px;
+                                    border-radius: 3px;
+                                    border: 1px solid lightgray;"
+                            >{menu_item_description}</textarea>
+                            <button
+                                type="submit"
+                                style="font-size: 16px;
+                                    margin: 10px;
+                                    background-color: #0083a8;
+                                    padding: 5px 10px;
+                                    border: 0;
+                                    border-radius: 3px;
+                                    color: white;
+                                    font-weight: 300;"
+                            >Update</button>
+                        </form>
+                        <a style="color: #0083a8; margin: 5px; font-weight: 400;" href="/restaurants/{restaurant_id}/">
+                            Cancel
+                        </a>
+                    </body>
+                </html>
+            '''
+        return page_html.format(
+            restaurant_id=restaurant_id,
+            restaurant_name=restaurant.name,
+            menu_item_id=menu_item_id,
+            menu_item_name=menu_item.name,
+            menu_item_price=menu_item.price,
+            menu_item_description=menu_item.description
+        )
 
 
 # Task 3: Create a route for deleteMenuItem function here
