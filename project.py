c1585cd46dbd393a635ace0da2d0494afd505e37
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-09-29 07:28:22 +0100

    feat: Created template for new menu item page

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -74,79 +74,9 @@ def new_menu_item(restaurant_id):
             menu_item = MenuItem(name=name, price=price, description=description, restaurant=restaurant)
             session.add(menu_item)
             session.commit()
-        return redirect('/restaurants/{restaurant_id}/'.format(restaurant_id=restaurant_id))
+        return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
     else:
-        page_html = '''
-            <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <title>Restaurants</title>
-                    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
-                </head>
-                <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
-                    <h1>{restaurant_name}</h1>
-                    <form style="margin-bottom: 10px;" method="POST" action="/restaurants/{restaurant_id}/new_menu_item/">
-                        <h2>Add menu item</h2>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Enter name"
-                            style="display: block;
-                                width: 100%;
-                                max-width: 500px;
-                                margin-bottom: 10px;
-                                font-size: 16px;
-                                padding: 5px;
-                                border-radius: 3px;
-                                border: 1px solid lightgray;"
-                        >
-                        <input
-                            type="text"
-                            name="price"
-                            placeholder="Enter price"
-                            style="display: block;
-                                width: 100%;
-                                max-width: 500px;
-                                margin-bottom: 10px;
-                                font-size: 16px;
-                                padding: 5px;
-                                border-radius: 3px;
-                                border: 1px solid lightgray;"
-                        >
-                        <textarea
-                            name="description"
-                            placeholder="Enter description"
-                            style="display: block;
-                                width: 100%;
-                                max-width: 500px;
-                                margin-bottom: 5px;
-                                font-size: 16px;
-                                padding: 5px;
-                                border-radius: 3px;
-                                border: 1px solid lightgray;"
-                        ></textarea>
-                        <button
-                            type="submit"
-                            style="font-size: 16px;
-                                margin: 10px;
-                                background-color: #0083a8;
-                                padding: 5px 10px;
-                                border: 0;
-                                border-radius: 3px;
-                                color: white;
-                                font-weight: 300;"
-                        >Create</button>
-                    </form>
-                    <a style="color: #0083a8; margin: 5px; font-weight: 400;" href="/restaurants/{restaurant_id}/">
-                        Cancel
-                    </a>
-                </body>
-            </html>
-        '''
-        return page_html.format(restaurant_id=restaurant_id, restaurant_name=restaurant.name)
-
+        return render_template('new_menu_item.html', restaurant=restaurant)
 
 # Task 2: Create route for editMenuItem function here
 
